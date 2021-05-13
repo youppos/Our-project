518f23230113d2c8824d1c62f80e1dad04c0c5b4
Nevermind131 <84068912+Nevermind131@users.noreply.github.com>
2021-05-13 20:25:21 +0800

    Update Demo.java

--- OurProject/src/Demo.java
+++ OurProject/src/Demo.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Demo {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
+        String ip="localhost";
         Register register1 = new Register();
         System.out.println("注册");
         System.out.print("输入账号：");
@@ -14,7 +15,7 @@ public class Demo {
         System.out.print("\n输入用户名：");
         String userName=input.nextLine();
         register1.setUserName(userName);
-        HandleRegister handleRegister1=new HandleRegister("localhost");
+        HandleRegister handleRegister1=new HandleRegister(ip);
         handleRegister1.writeRegister(register1);
         Login login1=new Login();
         System.out.println("\n登陆");
@@ -24,7 +25,7 @@ public class Demo {
         System.out.print("\n输入密码：");
         String password2=input.nextLine();
         login1.setPassword(password2);
-        HandleLogin handleLogin1=new HandleLogin("localhost");
+        HandleLogin handleLogin1=new HandleLogin(ip);
         handleLogin1.verify(login1);
     }
 }
