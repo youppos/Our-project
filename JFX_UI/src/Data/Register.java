e1f1b8e477d9170ad3e0ae8cd7130f16ec70611e
youppos <47168589+youppos@users.noreply.github.com>
2021-05-15 20:19:37 +0800

    Add files via upload

---
+++ JFX_UI/src/Data/Register.java
@@ -0,0 +1,31 @@
+package Data;
+
+public class Register {
+    private String id;
+    private String password;
+    private String userName;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
