188c6231b31abbbf5ecf5e509af35a583e52dfd7
youppos <47168589+youppos@users.noreply.github.com>
2021-05-20 09:51:54 +0800

    Add files via upload

---
+++ JFX_UI/src/Data/PersonDao.java
@@ -0,0 +1,76 @@
+package Data;
+
+import BaseData.Person;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonDao {
+
+
+    /**
+     * 操作结果：根据SQL语句执行数据库的增删改操作
+     *
+     * @param sql SQL语句
+     * @return boolean 如果操作数据库成功返回true，否则返回false
+     */
+    public boolean dataChange(String sql) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //获得数据的连接
+            conn = JDBCUtils.getConnection();
+            //获得Statement对象
+            stmt = conn.createStatement();
+            //发送SQL语句
+            int num = stmt.executeUpdate(sql);
+            return num > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.release(stmt, conn);
+        }
+        return false;
+    }
+
+    /**
+     * 操作结果：根据参数sql获取数据库记录数据
+     *
+     * @param sql SQL语句
+     * @return List 返回包含记录Records对象的集合
+     */
+    public List getRecordsDataBySql(String sql) {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        List list = new ArrayList();
+        try {
+            //获得数据的连接
+            conn = JDBCUtils.getConnection();
+            //获得Statement对象
+            stmt = conn.createStatement();
+            //发送SQL语句
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                Person person = new Person();
+                // 索引是从1开始的
+                person.setId(rs.getString(1));
+                person.setName(rs.getString(2));
+                person.setSex(rs.getString(3));
+                person.setAge(rs.getInt(4));
+                person.setWenhua(rs.getString(5));
+                person.setIncome(rs.getString(6));
+                person.setImportant(rs.getString(7));
+                list.add(person);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.release(rs, stmt, conn);
+        }
+        return list;
+    }
+}
